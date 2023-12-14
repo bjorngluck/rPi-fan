da25433f5945437e6ff5d6074f7af599dd633309
bjorn.gluck <bjorn.gluck@xneelo.com>
2023-12-14 17:54:34 +0200

    Updated service details and documentation

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # rPi-fan
 Collection of scripts to control fan speed of a Ubuntu Raspberry Pi 4
+
+
+https://medium.com/codex/setup-a-python-script-as-a-service-through-systemctl-systemd-f0cc55a42267
+
+
+Cretae a scripts folder
+mkdir scripts
+
+Change to the scripts folder and then create a fan_service.py file and dump the contents of the fan_service.py file
+cd scripts
+vi fan_service.py
+
+Create the service 
+sudo nano /etc/systemd/system/fan.service 
+and create the contents of the fan.service file
+
+Now we need to reload the daemon.
+
+sudo systemctl daemon-reload
+Let’s enable our service so that it doesn’t get disabled if the server restarts.
+
+sudo systemctl enable fan.service
+And now let’ start our service.
+
+sudo systemctl start fan.service
+
+There are several commands you can do to start, stop, restart, and check status.
+
+To stop the service.
+
+sudo systemctl stop fan.service
+To restart.
+
+sudo systemctl restart fan.service
+To check status.
+
+sudo systemctl status fan.service
---
+++ fan_service.py
@@ -0,0 +1,45 @@
+import RPi.GPIO as GPIO
+import time
+import subprocess as sp
+
+
+# initializing GPIO, setting mode to BOARD.
+# Default pin of FAN Adapter is physical pin 32, GPIO12;
+Fan = 32  #if you connect to pin txd physical pin 8, GPIO14，then set to :Fan = 8
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(Fan, GPIO.OUT)
+
+p = GPIO.PWM(Fan, 50)
+p.start(0)
+
+try:
+    while True:
+        temp = sp.getoutput("vcgencmd measure_temp|egrep -o '[0-9]*\.[0-9]*'")
+        # print(temp)
+        cpu = float(temp)
+        #print('CPU temperature is: %2.2f' % cpu)
+        if float(temp) < 40.0:
+            p.ChangeDutyCycle(0)
+            # print('Fan is off as CPU temperature is %2.2f' % cpu)
+            time.sleep(10)
+        elif float(temp) > 40.0 and float(temp) < 45.0:
+            p.ChangeDutyCycle(30)
+            # print('Fan is on 30 as CPU temperature is %2.2f' % cpu)
+            time.sleep(10)
+        elif float(temp) > 45.0 and float(temp) < 50.0:
+            p.ChangeDutyCycle(50)
+            # print('Fan is on 50 as CPU temperature is %2.2f' % cpu)
+            time.sleep(10)
+        elif float(temp) > 50.0 and float(temp) < 55.0:
+            p.ChangeDutyCycle(75)
+            # print('Fan is on 75 as CPU temperature is %2.2f' % cpu)
+            time.sleep(10)
+        elif float(temp) > 55.0:
+            p.ChangeDutyCycle(100)
+            # print('Fan is on 100 as CPU temperature is %2.2f' % cpu)
+            time.sleep(10)
+
+except KeyboardInterrupt:
+    pass
+p.stop()
+GPIO.cleanup()
